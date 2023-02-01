2de025e4599d3f769c6849aea485dd58e84f664d
Arpan-Asus <arpansaha004@gmail.com>
2023-02-01 12:21:35 +0530

    Some new files

---
+++ sort_in_Lexicographical_order.c
@@ -0,0 +1,13 @@
+// 61 . WAP to sort elemnts in lexicographical order (dictionary style )
+
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	char str[500];
+	printf("\nENTER THE STRING : ");
+	fgets(str,sizeof(str),stdin);
+
+	
+	return 0;
+}
